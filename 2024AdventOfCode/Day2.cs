d11f8e26d03bd2a7c0f9da64061ad3627e127c58
jwilson-hall <joseph.wilsonhall@gmail.com>
2024-12-05 22:48:44 +0000

    day 2

--- 2024AdventOfCode/Day2.cs
+++ 2024AdventOfCode/Day2.cs
@@ -7,18 +7,116 @@ using System.Threading.Tasks;
 namespace _2024AdventOfCode;
 internal class Day2
 {
-    public void LoadData()
+    public Day2Object LoadData()
     {
-        throw new NotImplementedException();
+        Day2Object day2Object = new Day2Object();
+        foreach (string line in File.ReadLines("../../../Day2Input.txt"))
+        {
+            var levels = line.Split(" ").Select(int.Parse).ToList();
+            day2Object.reports.Add(levels);
+        }
+        return day2Object;
     }
-    public void CalculateAnswer()
+    public int CalculateAnswerPart1(Day2Object day2Object)
     {
-        throw new NotImplementedException();
+        int safeReportsCount = 0;
+        foreach (List<int> item in day2Object.reports)
+        {
+            bool safeReport = true;
+            bool directionIncrease = item[1] > item[0];
+            for (int i = 0; i < item.Count-1; i++)
+            {
+                int diff = item[i] - item[i + 1];
+                if (Math.Abs(diff) is > 3 or 0)
+                {
+                    safeReport = false;
+                }
+                if (directionIncrease && diff > 0)
+                {
+                    safeReport = false;
+                }
+                else if(!directionIncrease && diff < 0)
+                {
+                    safeReport = false;
+                }
+            }
+            if (safeReport)
+            {
+                safeReportsCount++;
+            }
+        }
+        return safeReportsCount;
     }
 
+    public int CalculateAnswerPart2(Day2Object day2Object)
+    {
+        int safeReportsCount = 0;
+        foreach (List<int> item in day2Object.reports)
+        {
+            bool safeReport = true;
+            bool directionIncrease = item[1] > item[0];
+            for (int i = 0; i < item.Count - 1; i++)
+            {
+                int diff = item[i] - item[i + 1];
+                if (Math.Abs(diff) is > 3 or 0)
+                {
+                    safeReport = false;
+                }
+                if (directionIncrease && diff > 0)
+                {
+                    safeReport = false;
+                }
+                else if (!directionIncrease && diff < 0)
+                {
+                    safeReport = false;
+                }
+            }
+            if (safeReport)
+            {
+                safeReportsCount++;
+            }
+            else
+            {
+                for (int i = 0; i < item.Count; i++)
+                {
+                    var tempItem = new List<int>(item);
+                    tempItem.RemoveAt(i);
+                    if (IsSafeReport(tempItem))
+                    {
+                        safeReportsCount++;
+                        break;
+                    }
+                }
+            }
+        }
+        return safeReportsCount;
+    }
+
+    private bool IsSafeReport(List<int> item)
+    {
+        if (item.Count < 2) return false;
+        bool directionIncrease = item[1] > item[0];
+        for (int i = 0; i < item.Count - 1; i++)
+        {
+            int diff = item[i] - item[i + 1];
+            if (Math.Abs(diff) is > 3 or 0)
+            {
+                return false;
+            }
+            if (directionIncrease && diff > 0)
+            {
+                return false;
+            }
+            else if (!directionIncrease && diff < 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 public class Day2Object 
 {
-
+    public List<List<int>> reports = new();
 }
--- 2024AdventOfCode/UnitTest1.cs
+++ 2024AdventOfCode/UnitTest1.cs
@@ -7,25 +7,31 @@ public class UnitTest1(ITestOutputHelper output)
     [Fact]
     public void Day1()
     {
-        var day1 = new Day1();
-        var input = day1.LoadData();
+        var puzzle = new Day1();
+        var input = puzzle.LoadData();
 
-        int answer1 = day1.CalculateAnswerPart1(input);
+        int answer1 = puzzle.CalculateAnswerPart1(input);
         output.WriteLine($"Day 1 Part 1: {answer1}");
         Assert.Equal(765748, answer1 );
 
-        input = day1.LoadData();
-        int answer2 = day1.CalculateAnswerPart2(input);
+        input = puzzle.LoadData();
+        int answer2 = puzzle.CalculateAnswerPart2(input);
         output.WriteLine($"Day 1 Part 2: {answer2}");
         Assert.Equal(27732508, answer2);
     }
     [Fact]
     public void Day2()
     {
-        var day2 = new Day2();
-        var input = day2.LoadData();
+        var puzzle = new Day2();
+        var input = puzzle.LoadData();
 
-        //int answer = day2.TotalDistance(input);
-        //output.WriteLine($"Day 2: {answer}");
+        int answer1 = puzzle.CalculateAnswerPart1(input);
+        output.WriteLine($"Day 2 Part 1: {answer1}");
+        Assert.Equal(463, answer1);
+
+        input = puzzle.LoadData();
+        int answer2 = puzzle.CalculateAnswerPart2(input);
+        output.WriteLine($"Day 2 Part 2: {answer2}");
+
     }
 } 
